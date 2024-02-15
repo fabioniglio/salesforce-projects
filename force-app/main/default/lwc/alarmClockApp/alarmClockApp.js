720fbd71e9f546a1fbff3adbdf8d5e0291bed459
Fabio Niglio <fabio_niglio@hotmail.com>
2024-02-15 16:43:51 +0100

    alarm logic and reset alarm

--- force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-empty */
 /* eslint-disable @lwc/lwc/no-async-operation */
 import { LightningElement } from "lwc";
 import AlarmClockAssets from "@salesforce/resourceUrl/AlarmClockAssets";
@@ -8,6 +9,17 @@ export default class AlarmClockApp extends LightningElement {
   hours = [];
   minutes = [];
   meridiems = ["AM", "PM"];
+  alarmTime;
+  isAlarmSet;
+
+  hourSelected;
+  minSelected;
+  meridiemSelected;
+
+  get isFieldNotSelected() {
+    return !(this.hourSelected && this.minSelected && this.meridiemSelected);
+  }
+
   connectedCallback() {
     this.currentTimeHandler();
     this.createHoursOptions();
@@ -37,12 +49,11 @@ export default class AlarmClockApp extends LightningElement {
       sec = sec < 10 ? "0" + sec : sec;
 
       this.currentTime = `${hour}:${min}:${sec} ${ampm}`;
-
       if (this.alarmTime === `${hour}:${min} ${ampm}`) {
         console.log("Alarm Triggered!!");
-        this.isAlarmTriggered = true;
-        this.ringtone.play();
-        this.ringtone.loop = true;
+        //   this.isAlarmTriggered = true;
+        //   this.ringtone.play();
+        //   this.ringtone.loop = true;
       }
     }, 1000);
   }
@@ -58,5 +69,36 @@ export default class AlarmClockApp extends LightningElement {
       let val = i < 10 ? "0" + i : i;
       this.minutes.push(val);
     }
+  }
+
+  optionhandler(event) {
+    const { label, value } = event.detail;
+
+    if (label === "Hour(s)") {
+      this.hourSelected = value;
+    } else if (label === "Minute(s)") {
+      this.minSelected = value;
+    } else if (label === "AM/PM") {
+      this.meridiemSelected = value;
+    } else {
+    }
+
+    console.log(" this.hourSelected", this.hourSelected);
+    console.log(" this.minSelected", this.minSelected);
+    console.log(" this.meridiemSelected", this.meridiemSelected);
+  }
+
+  setAlarmHandler() {
+    this.alarmTime = `${this.hourSelected}:${this.minSelected} ${this.meridiemSelected}`;
+    this.isAlarmSet = true;
+  }
+
+  clearAlarmHandler() {
+    this.alarmTime = "";
+    this.isAlarmSet = false;
+
+    this.hourSelected = "";
+    this.minSelected = "";
+    this.meridiemSelected = "";
   }
 }
