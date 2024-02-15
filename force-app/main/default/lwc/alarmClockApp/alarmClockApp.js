823975b0f43f1a1d3ac1fc420b88f27f764c09ad
Fabio Niglio <fabio_niglio@hotmail.com>
2024-02-15 15:34:54 +0100

    bug fix 12 pm

--- force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -12,25 +12,33 @@ export default class AlarmClockApp extends LightningElement {
   currentTimeHandler() {
     setInterval(() => {
       let dateTime = new Date();
-
       let hour = dateTime.getHours();
-
       let min = dateTime.getMinutes();
       let sec = dateTime.getSeconds();
-
       let ampm = "AM";
 
-      if (hour == 0) {
+      if (hour === 0) {
         hour = 12;
+        ampm = "AM";
+      } else if (hour === 12) {
+        ampm = "PM";
       } else if (hour >= 12) {
         hour = hour - 12;
-        ampm = "pm";
+        ampm = "PM";
       }
+
       hour = hour < 10 ? "0" + hour : hour;
       min = min < 10 ? "0" + min : min;
       sec = sec < 10 ? "0" + sec : sec;
 
       this.currentTime = `${hour}:${min}:${sec} ${ampm}`;
+
+      if (this.alarmTime === `${hour}:${min} ${ampm}`) {
+        console.log("Alarm Triggered!!");
+        this.isAlarmTriggered = true;
+        this.ringtone.play();
+        this.ringtone.loop = true;
+      }
     }, 1000);
   }
 }
