dd0044ea7b8df8c0985fd331fd6b935e9a1fc002
Fabio Niglio <fabio_niglio@hotmail.com>
2024-02-25 00:07:55 +0100

    adding input lightning fields

--- force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
+++ force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
@@ -1,3 +1,58 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class NoteTakingApp extends LightningElement {}
+const DEFAULT_NOTE_FORM = {
+  Name: "",
+  Note_Description__c: ""
+};
+
+export default class NoteTakingApp extends LightningElement {
+  showModal = false;
+
+  noteRecord = DEFAULT_NOTE_FORM;
+
+  formats = [
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "list",
+    "indent",
+    "align",
+    "link",
+    "clean",
+    "table",
+    "header",
+    "color"
+  ];
+
+  get isFormInvalid() {
+    return !(
+      this.noteRecord &&
+      this.noteRecord.Note_Description__c &&
+      this.noteRecord.Name
+    );
+  }
+
+  createNoteHandler() {
+    this.showModal = true;
+  }
+
+  closeModalHandler() {
+    this.showModal = false;
+
+    this.noteRecord = DEFAULT_NOTE_FORM;
+  }
+
+  changeHandler(event) {
+    const { name, value } = event.target;
+
+    this.noteRecord = { ...this.noteRecord, [name]: value };
+  }
+
+  formSubmitHandler(event) {
+    event.preventDefault();
+    console.log("this.noteRecord", JSON.stringify(this.noteRecord));
+  }
+}
